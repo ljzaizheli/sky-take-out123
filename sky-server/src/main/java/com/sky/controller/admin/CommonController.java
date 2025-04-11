e98718a487b0945f8fc9fcca910ce79236487ff8
lijun <lj20049595@qq.com>
2025-04-12 02:23:35 +0800

    菜品分页查询

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -3,6 +3,9 @@ package com.sky.controller.admin;
 import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
 import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +17,15 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/admin/common")
+@Slf4j
+@Api(tags = "文件上传相关接口")
 public class CommonController {
 
     @Autowired
     private AliOssUtil ossUtil;
 
     @PostMapping("/upload")
+    @ApiOperation("文件上传")
     public Result<String> upload(MultipartFile file) {
         try {
             String fileName = file.getOriginalFilename();
